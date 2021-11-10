f5599c74c52baa73ce0e38fd2d985841a71a909e
Fabian Jochen Kanzler <hello@fabianjochenkanzler>
2021-11-10 09:53:36 +0100

    second commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO_URI/* , { useNewUrlParser: true, useUnifiedTopolog: true} */)
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopolog: true})
 
 
 let Person;
